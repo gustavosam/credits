57b8c9761404f9ae32b1da55b4dfbe210e8d4f45
Luis Gustavo Sebastian Sam <gustavosebastiansam@outlook.com>
2023-09-18 16:17:28 -0500

    El MS Credit se registra en el servidiro Eureka

--- src/main/java/com/microservice/credit/CreditApplication.java
+++ src/main/java/com/microservice/credit/CreditApplication.java
@@ -3,11 +3,13 @@ package com.microservice.credit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Aplicación principal del microservicio de creditos.
  * */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CreditApplication {
 
